11d54282ff1deb4a220c5397703c908d6c7dc911
Abdullin Ildar <abdullintat@yandex.ru>
2022-07-27 20:20:11 +0300

    news landing

---
+++ header.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-exp.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-icons.min.css">
+
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"> -->
+
+    <link rel="stylesheet" href="css/ui-kit.css">
+
+    <title>Главная</title>
+</head>
+
+<body>
+    <div class="main_header">
+        <div class="additional_top_menu ai-bg-gray">
+            <div class="container grid-xl">
+                <div class="columns">
+                    <div class="column col-ai-auto">
+                        <a href="#" class="">
+                            <div class="ai-icon ai-text-sm ai-text-tertiary">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17.657 16.657L13.414 20.9C13.2284 21.0858 13.0081 21.2331 12.7656 21.3336C12.523 21.4342 12.2631 21.4859 12.0005 21.4859C11.738 21.4859 11.478 21.4342 11.2354 21.3336C10.9929 21.2331 10.7726 21.0858 10.587 20.9L6.343 16.657C5.22422 15.5382 4.46234 14.1127 4.15369 12.5609C3.84504 11.009 4.00349 9.40053 4.60901 7.93874C5.21452 6.47696 6.2399 5.22755 7.55548 4.34852C8.87107 3.46949 10.4178 3.00031 12 3.00031C13.5822 3.00031 15.1289 3.46949 16.4445 4.34852C17.7601 5.22755 18.7855 6.47696 19.391 7.93874C19.9965 9.40053 20.155 11.009 19.8463 12.5609C19.5377 14.1127 18.7758 15.5382 17.657 16.657V16.657Z" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M14.1213 13.1213C14.6839 12.5587 15 11.7956 15 11C15 10.2044 14.6839 9.44129 14.1213 8.87868C13.5587 8.31607 12.7956 8 12 8C11.2044 8 10.4413 8.31607 9.87868 8.87868C9.31607 9.44129 9 10.2044 9 11C9 11.7956 9.31607 12.5587 9.87868 13.1213C10.4413 13.6839 11.2044 14 12 14C12.7956 14 13.5587 13.6839 14.1213 13.1213Z" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <div class="ai-text-tertiary ai-text-bold">Альметьевск</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="column col-auto">
+                        <a href="#" class="ai-text-sm ai-text-dark">Помощь</a>
+                    </div>
+                    <div class="column col-auto">
+                        <a href="#" class="ai-text-sm ai-text-dark">Тарифы</a>
+                    </div>
+                    <div class="column col-auto">
+                        <a href="#" class="ai-text-sm ai-text-dark">Для бизнеса</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="container grid-xl">
+            <div class="columns ai-h-flex pt-2 pb-2">
+                <div class="column col-auto">
+                    <a href="index.php" class="navbar-brand">
+                        <img class="ai-logo" src="img/logo_lg.svg" alt="">
+                    </a>
+                </div>
+                <div class="column col-auto">
+                    <button class="btn ai-bg-primary btn-lg ai-text-white">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M21 5H3M21 12H3M12 19H3" stroke-linecap="round" />
+                        </svg>
+                        Каталог
+                    </button>
+                </div>
+                <div class="column col-4 col-mx-auto">
+                    <div class="input-group">
+                        <input type="text" class="form-input" placeholder="Поиск по справочнику">
+                        <button class="btn ai-bg-primary input-group-btn ai-text-white">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M21 21L15 15M17 10C17 10.9193 16.8189 11.8295 16.4672 12.6788C16.1154 13.5281 15.5998 14.2997 14.9497 14.9497C14.2997 15.5998 13.5281 16.1154 12.6788 16.4672C11.8295 16.8189 10.9193 17 10 17C9.08075 17 8.1705 16.8189 7.32122 16.4672C6.47194 16.1154 5.70026 15.5998 5.05025 14.9497C4.40024 14.2997 3.88463 13.5281 3.53284 12.6788C3.18106 11.8295 3 10.9193 3 10C3 8.14348 3.7375 6.36301 5.05025 5.05025C6.36301 3.7375 8.14348 3 10 3C11.8565 3 13.637 3.7375 14.9497 5.05025C16.2625 6.36301 17 8.14348 17 10Z" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div class="column col-auto">
+                    <button class="btn ai-bg-tertiary btn-lg rounded ai-text-white">                        
+                        &nbsp;ДОБАВИТЬ
+                    </button>
+                </div>
+                <div class="column col-auto">
+                    <a href="#" class="ai-vert-icon ai-text-dark ai-text-mc ai-text-bold">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M14.8284 9.82843C15.5786 9.07828 16 8.06087 16 7C16 5.93913 15.5786 4.92172 14.8284 4.17157C14.0783 3.42143 13.0609 3 12 3C10.9391 3 9.92172 3.42143 9.17157 4.17157C8.42143 4.92172 8 5.93913 8 7C8 8.06087 8.42143 9.07828 9.17157 9.82843C9.92172 10.5786 10.9391 11 12 11C13.0609 11 14.0783 10.5786 14.8284 9.82843Z" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M7.05025 16.0503C8.36301 14.7375 10.1435 14 12 14C13.8565 14 15.637 14.7375 16.9497 16.0503C18.2625 17.363 19 19.1435 19 21H5C5 19.1435 5.7375 17.363 7.05025 16.0503Z" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        Войти
+                    </a>
+                </div>
+                <div class="column col-auto">
+                    <a href="#" class="ai-vert-icon ai-text-dark ai-text-mc ai-text-bold">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M3 12C3 10.1144 3 9.17157 3.58579 8.58579C4.17157 8 5.11438 8 7 8H17C18.8856 8 19.8284 8 20.4142 8.58579C21 9.17157 21 10.1144 21 12V16C21 17.8856 21 18.8284 20.4142 19.4142C19.8284 20 18.8856 20 17 20H7C5.11438 20 4.17157 20 3.58579 19.4142C3 18.8284 3 17.8856 3 16V12Z" stroke="white" stroke-width="2" />
+                            <path d="M18 8L17.539 6.92432C17.0351 5.74853 16.7831 5.16064 16.2822 4.83032C15.7812 4.5 15.1416 4.5 13.8624 4.5H7C5.11438 4.5 4.17157 4.5 3.58579 5.08579C3 5.67157 3 6.61438 3 8.5V15" stroke="white" stroke-width="2" />
+                            <rect x="16.5" y="11" width="2" height="2" rx="1" fill="white" />
+                        </svg>
+                        Заказы
+                    </a>
+                </div>
+                <div class="column col-auto">
+                    <a href="#" class="ai-vert-icon ai-text-dark ai-text-mc ai-text-bold">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M3.34255 7.77789C3.5687 7.23192 3.90017 6.73584 4.31804 6.31798C4.7359 5.90011 5.23198 5.56864 5.77795 5.34249C6.32392 5.11634 6.90909 4.99994 7.50004 4.99994C8.09099 4.99994 8.67616 5.11634 9.22213 5.34249C9.7681 5.56864 10.2642 5.90011 10.682 6.31798L12 7.63598L13.318 6.31798C14.162 5.47406 15.3066 4.99995 16.5 4.99995C17.6935 4.99995 18.8381 5.47406 19.682 6.31798C20.526 7.1619 21.0001 8.3065 21.0001 9.49998C21.0001 10.6935 20.526 11.8381 19.682 12.682L12 20.364L4.31804 12.682C3.90017 12.2641 3.5687 11.768 3.34255 11.2221C3.1164 10.6761 3 10.0909 3 9.49998C3 8.90902 3.1164 8.32386 3.34255 7.77789Z" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        Избранное
+                    </a>
+                </div>
+                <div class="column col-auto">
+                    <a href="#" class="ai-vert-icon ai-text-dark ai-text-mc ai-text-bold">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M3 3H5L5.4 5M5.4 5H21L17 13H7M5.4 5L7 13M7 13L4.707 15.293C4.077 15.923 4.523 17 5.414 17H17M17 17C16.4696 17 15.9609 17.2107 15.5858 17.5858C15.2107 17.9609 15 18.4696 15 19C15 19.5304 15.2107 20.0391 15.5858 20.4142C15.9609 20.7893 16.4696 21 17 21C17.5304 21 18.0391 20.7893 18.4142 20.4142C18.7893 20.0391 19 19.5304 19 19C19 18.4696 18.7893 17.9609 18.4142 17.5858C18.0391 17.2107 17.5304 17 17 17ZM9 19C9 19.5304 8.78929 20.0391 8.41421 20.4142C8.03914 20.7893 7.53043 21 7 21C6.46957 21 5.96086 20.7893 5.58579 20.4142C5.21071 20.0391 5 19.5304 5 19C5 18.4696 5.21071 17.9609 5.58579 17.5858C5.96086 17.2107 6.46957 17 7 17C7.53043 17 8.03914 17.2107 8.41421 17.5858C8.78929 17.9609 9 18.4696 9 19Z" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        Корзина
+                    </a>
+                </div>
+            </div>
+            <div class="ai-h-flex pt-2 pb-2">
+                <a href="index.php" class="ai-text-sm ai-text-dark ai-text-bold ai-text-caps">Главная</a>
+                <a href="#" class="ai-text-sm ai-text-dark ai-text-bold ai-text-caps">Справочник</a>
+                <a href="#" class="ai-text-sm ai-text-dark ai-text-bold ai-text-caps">Новости</a>
+                <a href="#" class="ai-text-sm ai-text-dark ai-text-bold ai-text-caps">Кино</a>
+                <a href="#" class="ai-text-sm ai-text-dark ai-text-bold ai-text-caps">Афиша</a>
+                <a href="#" class="ai-text-sm ai-text-dark ai-text-bold ai-text-caps">Работа</a>
+                <a href="#" class="ai-text-sm ai-text-dark ai-text-bold ai-text-caps">Объявления</a>
+                <a href="#" class="ai-text-sm ai-text-dark ai-text-bold ai-text-caps">Акции и скидки</a>
+            </div>
+        </div>
+    </div>
---
+++ ui.php
@@ -0,0 +1,29 @@
+<div class="d-flex" style="gap: 12px;">
+    <div class="cube ai-bg-primary ai-text-primary">Ура</div>
+    <div class="cube ai-bg-secondary ai-text-secondary">Ура</div>
+    <div class="cube ai-bg-tertiary ai-text-tertiary">Ура</div>
+    <div class="cube ai-bg-info ai-text-info">Ура</div>
+    <div class="cube ai-bg-success ai-text-success">Ура</div>
+    <div class="cube ai-bg-warning ai-text-warning">Ура</div>
+    <div class="cube ai-bg-error ai-text-error">Ура</div>
+    <div class="cube ai-bg-critical ai-text-critical">Ура</div>
+    <div class="cube ai-bg-dark ai-text-dark">Ура</div>
+    <div class="cube ai-bg-gray ai-text-gray">Ура</div>
+    <div class="cube ai-bg-light ai-text-light">Ура</div>
+    <div class="cube ai-bg-white ai-text-white">Ура</div>
+</div>
+<div class="d-flex" style="gap: 12px;">
+    <div class="cube ai-text-primary">Ура</div>
+    <div class="cube ai-text-secondary">Ура</div>
+    <div class="cube ai-text-tertiary">Ура</div>
+    <div class="cube ai-text-info">Ура</div>
+    <div class="cube ai-text-success">Ура</div>
+    <div class="cube ai-text-warning">Ура</div>
+    <div class="cube ai-text-error">Ура</div>
+    <div class="cube ai-text-critical">Ура</div>
+    <div class="cube ai-text-dark">Ура</div>
+    <div class="cube ai-text-gray">Ура</div>
+    <div class="cube ai-text-light">Ура</div>
+    <div class="cube ai-text-white">Ура</div>
+</div>
+
