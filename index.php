d6111b78d5b5a4272ab17bd4bbd2e1ce9f0247e8
Abdullin Ildar <abdullintat@yandex.ru>
2022-08-14 17:55:28 +0300

    add ai-card-log & card-owner classes

--- index.php
+++ index.php
@@ -5,10 +5,13 @@ include_once('header.php');
 <div class="container content-container grid-xl ai-bg-white">
 
     <h1 class="ai-title-primary">Содержание</h1>
-
     <ul>
-        <li><a href="news_landing.php">Новости (лендинг) НАЖМИ МЕНЯ!</a></li>
-        
+        <li><a href="news_landing.php">Новости (лендинг)</a></li>
+
+    </ul>
+    <ul>
+        <li><a href="post_landing.php">Бизнес-лента (лендинг)</a></li>
+
     </ul>
 </div>
 </body>
