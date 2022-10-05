2d41e0724a8fbb2dab079e18ec95335d254d4a50
Ildar <abdullintat@yandex.ru>
2022-10-05 22:22:57 +0300

    Изменил кнопки с button на a ДОбавил wrapper, main, section, footer Стилизовал wrapper и main.

---
+++ main-menu_mobile.php
@@ -0,0 +1,40 @@
+<?php
+include_once('nav_bar.php');
+?>
+
+<div class="wrapper container grid-lg">
+    <main class="">
+
+        <h1 class="ai-title-primary">Содержание</h1>
+        <ul>
+            <li><a href="news_landing.php">Новости (лендинг)</a></li>
+
+        </ul>
+        <ul>
+            <li><a href="news_one.php">Новость (одна)</a></li>
+
+        </ul>
+        <ul>
+            <li><a href="post_landing.php">Бизнес-лента (лендинг)</a></li>
+
+        </ul>
+        <ul>
+            <li><a href="post_one.php">Пост Бизнес-ленты (один)</a></li>
+
+        </ul>
+        <ul>
+            <li><a href="movie_landing.php">Афиша кино (лендинг)</a></li>
+
+        </ul>
+
+
+    </main>
+
+</div>
+
+
+
+
+<?php
+include_once('footer_bar.php');
+?>
